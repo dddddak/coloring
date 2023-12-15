85d378e0e9c386035275f8bd52921ba235ec2cff
dddddak <132284730+dddddak@users.noreply.github.com>
2023-12-14 23:44:32 -0500

    Add files via upload

---
+++ coloring.js
@@ -0,0 +1,17 @@
+background(186, 145, 20); // wooden table
+ellipse(200, 200, 350, 350); // plate
+ellipse(200, 200, 300, 300); 
+
+fill(255, 230, 0);
+noStroke();
+rect(110, 120, 70, 70);
+
+fill(255, 0, 0);
+triangle(270, 300, 230, 200, 300, 230);
+
+fill(145, 0, 255);
+ellipse(150, 250, 100, 80);
+
+strokeWeight(33);
+stroke(97, 46, 46);
+line(300, 200, 230, 100); 
